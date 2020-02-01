9ca193891976483a09baef37dc7faafe889b1681
Aaron Lain <aaron.j.lain@gmail.com>
2020-02-01 11:10:42 -0600

    expell working

--- main.js
+++ main.js
@@ -1,39 +1,26 @@
 const studentArray = [];
 
-
-const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
-}
-
 const events = () => {
-    
-    // document.getElementById('btn-main').addEventListener('click', clearDom('jumbotron'));
+    document.getElementById('btn-main').addEventListener('click', () => {
+        clearDom('jumbotron');
+    });
     document.getElementById('btn-sort').addEventListener('click', newStudent);
     document.getElementById('btn-sort').addEventListener('click', buildStudentCards);
 };
 
 const clearDom = (divId) => {
-    let element = document.getElementById(divId);
+    const element = document.getElementById(divId);
     element.parentNode.removeChild(element);
 };
 
-const expellStudent = (e) => {
-    buttonId = e.target.id;
-    clearDom(buttonId);
-}
-//const showForm = () => {
-//    clearDom('jumbotron');
-//    let domString = '';
-//     <form>`
-//        <div class="d-flex flex-column justify-content-center w-50 p2 around form-group">`
-//            <label for="Name">Student's Name</label>`
-//            <input type="text" class="form-control" id="inputName" aria-describedby="emailHelp" placeholder="Enter Name">`
-//        </div>`
-//            <button type="submit" id="btn-sort" class="btn btn-success">Sort!</button>`
-//     </form>`
-//    printToDom('input-form', domString);
-//};
+const newStudent = () => {
+    let student = new Object();
+    student.name = document.getElementById('inputName').value;
+    student.id = Math.ceil(Math.random() * 124568)
+    houseGenerator(student);
+    studentArray.push(student);
+    console.log(studentArray);
+};
 
 const houseGenerator = (obj) => {
     switch (Math.ceil(Math.random() * 4)) {
@@ -51,30 +38,38 @@ const houseGenerator = (obj) => {
     }
 }
 
-const newStudent = () => {
-    let student = new Object();
-    student.name = document.getElementById('inputName').value;
-    student.id = Math.ceil(Math.random() * 124568)
-    houseGenerator(student);
-    studentArray.push(student);
-    console.log(studentArray);
-};
-
-
 const buildStudentCards = () => {
     let domString = ''; 
     studentArray.forEach((student) => {
-        domString += `<div class="card" style="width: 18rem;">`
+        domString += `<div class="card d-flex flex-column justify-content-center" style="width: 30%;">`
         domString += `<div class="card-body">`
         domString +=   `<h5 class="card-title">${student.name}</h5>`
         domString +=   `<p class="card-text">${student.house}</p>`
-        domString +=   `<a href="#" id=${student.id} class="btn btn-primary expell">Expell</a>`
+        domString +=   `<a href="#" id="${student.id}" class="btn btn-danger expell">Expell</a>`
         domString += `</div>`
         domString += `</div>`
     });
     printToDom('student-cards', domString);
+    expellButtonLoop();
 };
 
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+const expellButtonLoop = () => {
+    const expelledStudentButtons = document.getElementsByClassName('expell');
+    for (let i = 0; i < expelledStudentButtons.length; i++) {
+        expelledStudentButtons[i].addEventListener('click', expellStudent);
+    }
+}
+
+const expellStudent = (e) => {
+    const buttonId = e.target.offsetParent;
+    buttonId.remove();
+}
 
 events();
+
 console.log(studentArray)
